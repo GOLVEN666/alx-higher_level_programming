f27b3aa63d0feedd940c59f4b56b630f1807ce70
ACH <78966503+GOLVEN666@users.noreply.github.com>
2024-01-17 01:21:42 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for i in range(97, 123):
     if chr(i) != 'q' and chr(i) != 'e':
-        print("{}".format(chr(i)), end="", sep=""#!/usr/bin/python3
-                for i in range(97, 123):
-                    if chr(i) != 'q' and chr(i) != 'e':
-                            print("{}".format(chr(i)), end="", sep="")
+        print("{}".format(chr(i)), end="", sep="")
                         
