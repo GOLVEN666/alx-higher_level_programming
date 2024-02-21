a0bb339a3a745dfb291fb0ba0f4a01e3860388c8
ACH <78966503+GOLVEN666@users.noreply.github.com>
2024-02-21 02:56:37 +0100

    0x0B-100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -2,8 +2,6 @@
 
 """
 This module contains function that inserts a line of text
-after each line containing a specific string (keyword)
-in a file.
 """
 
 
@@ -13,7 +11,7 @@ def append_after(filename="", search_string="", new_string=""):
     Args:
         filenane: The file to read and write to
         search_string: The string to search
-        new_string: The new string to add to the file
+        new_string: The new string to add to the file 100-append_after.py
     """
 
     with open(filename, mode="r+", encoding="utf-8") as f:
