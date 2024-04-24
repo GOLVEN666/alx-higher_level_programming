9f208d46561007fea4c7310a46164daa0abe47e8
ACH <78966503+GOLVEN666@users.noreply.github.com>
2024-04-24 01:18:18 +0100

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-const num = Math.floor(Number(process.argv[2]));
-
-console.log(!isNaN(num) ? `My number: ${num}` : "Not a number");
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-const num = Math.floor(Number(process.argv[2]));
-if (!isNaN(num)) {
-	for (let i=0; i<num; i++) {
-		console.log('C is fun');
-	}
-} else {
-	console.log('Missing number of occurrences');
-}
